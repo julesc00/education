2bbde8e49d1252ec2c91a8afd882184745162518
Julio Briones <Julio_Briones@epam.com>
2022-02-03 16:18:47 -0600

    Finished chapter 13, errors.

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,20 +1,55 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 # Consumers are equivalent to Django views for async apps.
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        # Accept connection
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_group_name = None
+        self.id = None
+        self.user = None
 
-    def disconnect(self, close_code):
-        pass
+    async def connect(self):
+        self.user = self.scope["user"]
+        self.id = self.scope["url_route"]["kwargs"]["course_id"]
+        self.room_group_name = "chat_%s" % self.id
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        # Accept connection
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        now = timezone.now()
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+                "user": self.user.username,
+                "datetime": now.isoformat(),
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        # Send message to Websocket
+        await self.send(text_data=json.dumps(event))
--- education/settings.py
+++ education/settings.py
@@ -175,3 +175,12 @@ CACHES = {
 }
 
 ASGI_APPLICATION = "education.routing.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
